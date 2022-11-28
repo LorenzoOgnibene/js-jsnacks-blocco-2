ad915b5ecfa9a2425a708c599fc5e91cd0d1d50a
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2022-11-28 13:12:30 +0100

    edit

--- snack_6/js/javascript.js
+++ snack_6/js/javascript.js
@@ -5,7 +5,7 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 
 let sum = 0;
 
 for (let i = 0; i < numbers.length; i++){
-    if( numbers.indexOf(i) % 2 === 1){
+    if( i % 2 === 1){
         sum += numbers[i];
     }
 }
