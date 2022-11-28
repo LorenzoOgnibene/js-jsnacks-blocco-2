1dcb6dadd0bdc3faa11f0f680ea1f0cd1fade429
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2022-11-28 12:42:19 +0100

    snack 6

---
+++ snack_6/js/javascript.js
@@ -0,0 +1,13 @@
+// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++){
+    if( numbers.indexOf(i) % 2 === 1){
+        sum += i;
+    }
+}
+
+console.log(sum);
